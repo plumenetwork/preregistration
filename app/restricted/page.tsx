66e93dfa1659c789bed06aa8593c186d183ca0fc
tea.x.random <tea.x.random@gmail.com>
2025-01-10 22:49:52 -0800

    geolocation

---
+++ app/restricted/page.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const RestrictedPage = () => {
+  return (
+    <div className="flex items-center justify-center">
+      <div className="flex flex-col gap-4 justify-center text-center max-w-[560px] w-full mt-[100px] border border-[#F0F0F0] bg-[#FFFFFF] rounded-[16px] p-10">
+        <Image
+          src="/images/plume-unavailable.avif"
+          className="mx-auto"
+          alt=""
+          width={48}
+          height={48}
+        />
+        <div className="font-semibold text-[20px]">PlumeDrop Unavailable</div>
+        <div className="text-[#515154]">
+          This program is unavailable to users in your region due to regulatory
+          measures.
+        </div>
+        <div>
+          <Link
+            href="/"
+            className="font-[500] text-sm border border-[#F0F0F0] rounded-full py-2 px-4"
+            target="_blank"
+          >
+            Learn more
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RestrictedPage;
---
+++ middleware.ts
@@ -0,0 +1,61 @@
+import { NextRequest, NextResponse } from "next/server";
+import { geolocation } from "@vercel/functions";
+import { get } from "@vercel/edge-config";
+
+const blockedCountries = [
+  "AF", // Afghanistan
+  "BY", // Belarus
+  "CF", // Central African Republic
+  "CU", // Cuba
+  "CD", // Democratic Republic of Congo
+  "KP", // Democratic People’s Republic of North Korea
+  "UA", // Donetsk People's Republic (DNR) region of Ukraine (use Ukraine code)
+  "IR", // Islamic Republic of Iran
+  "LR", // Liberia
+  "MZ", // Mozambique
+  "MM", // Myanmar
+  "UA", // Luhansk People's Republic (LNR) region of Ukraine (use Ukraine code)
+  "RW", // Rwanda
+  "SO", // Somalia
+  "SS", // South Sudan
+  "SD", // Sudan (North)
+  "SY", // Syria
+  "UG", // Uganda
+  "UA", // The Crimea (use Ukraine code)
+  "ZW", // Zimbabwe
+  "US", // The United States of America
+];
+
+export async function middleware(request: NextRequest) {
+  const { country } = geolocation(request);
+
+  const isGeoBlockingEnabled = await get("isGeoBlockingEnabled");
+  const isUrlRestricted = new URL(request.url).pathname.startsWith(
+    "/restricted"
+  );
+  const isApiUrl = new URL(request.url).pathname.startsWith("/api");
+  const isImages = new URL(request.url).pathname.startsWith("/images");
+
+  if (
+    !isUrlRestricted &&
+    !isImages &&
+    isGeoBlockingEnabled &&
+    country &&
+    blockedCountries.includes(country)
+  ) {
+    if (isApiUrl) {
+      return NextResponse.json(
+        { error: "This API is restricted in your country" },
+        { status: 403 }
+      );
+    }
+
+    return NextResponse.redirect(new URL("/restricted", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/((?!_next|static|favicon.ico).*)"],
+};
