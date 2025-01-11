0bbfbae4e3a57cc7f844f6ed0b2d9caa079f1151
tea.x.random <tea.x.random@gmail.com>
2025-01-10 22:51:59 -0800

    switch to mainnet

--- app/lib/wagmi.ts
+++ app/lib/wagmi.ts
@@ -1,10 +1,10 @@
 import "@rainbow-me/rainbowkit/styles.css";
-import { plume } from "wagmi/chains";
+import { mainnet } from "wagmi/chains";
 
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 
 export const config = getDefaultConfig({
   appName: "Plume",
   projectId: "04cc015520f075c8d0d0b4ed6e27a764",
-  chains: [plume],
+  chains: [mainnet],
 });
