d03e5dd94bd471d764f687396d6a47c79d454216
Silvestre Herrera <hello@silvestre.io>
2025-05-26 10:23:51 -0300

    Make /terms the new home page. Get rid of everything else. (#1)

--- app/api/discord/disconnect/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-import { clearDiscordCookies } from "@/app/lib/server/discord";
-
-export const POST = async () => {
-  await clearDiscordCookies();
-
-  return NextResponse.json({ ok: true }, { status: 200 });
-};
